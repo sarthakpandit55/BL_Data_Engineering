b8c314197a5644df8fd8039d32468792c33626ae
sarthakpandit55 <sarthakpandit55@gmail.com>
2026-05-30 11:30:21 +0530

    learn about offset and aggregate function

---
+++ Practice/aggregate_functions.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(emp_id) AS total_emp
+FROM EMPLOYEE;
+
+SELECT SUM(salary) AS total_salary
+FROM EMPLOYEE;
+
+SELECT AVG(salary) AS average_salary
+FROM EMPLOYEE;
+
+SELECT MIN(salary) AS min_salary
+FROM EMPLOYEE;
+
+SELECT MAX(salary) AS max_salary
+FROM EMPLOYEE;
